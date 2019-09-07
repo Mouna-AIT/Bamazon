899708a43af62c9524c21dc5ca76e2c7da594e23
Moumix <mouna.aitichou@gmail.com>
2019-09-07 17:07:39 -0400

    added dependencies and connection to mysql

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,3 +1,4 @@
+// Dependencies
 var inquirer = require('inquirer');
 var mysql = require('mysql');
 var Table = require('cli-table');
@@ -7,7 +8,9 @@ var connection = mysql.createConnection({
     port: 3306,
     user: "root",
     password: "Moumix123",
-    database: "Bamazon_db"
+    database: "Bamazon"
 })
 connection.connect(function(err) {
-            console.log("Using Port: " + connection.threadId);
+    if (err) throw err;
+    console.log("Using Port: " + connection.threadId)
+})
