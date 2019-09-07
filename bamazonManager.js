b48e9b8b1ad8140997de7daa3e97f573dddb1399
Moumix <mouna.aitichou@gmail.com>
2019-09-07 18:30:40 -0400

    function displays the low invent when less than 3

--- bamazonManager.js
+++ bamazonManager.js
@@ -12,8 +12,10 @@ var connection = mysql.createConnection({
 })
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("Using Port: " + connection.threadId)
-})
+    console.log("");
+    console.log("Hello, Welcome to Bamazon Manager!");
+    console.log("");
+});
 
 // Main manager prompt which runs for the manager file which has many diffrent options to choose from
 
@@ -21,7 +23,7 @@ var managerPrompt = function() {
     inquirer.prompt({
         name: "action",
         type: "list",
-        message: "Hello! What would you like to do?",
+        message: "What would you like to do?",
         choices: ["View products for sale", "View low inventory", "Add to inventory", "Add a new product", "Exit"]
     }).then(function(answer) {
         switch (answer.action) {
@@ -50,3 +52,49 @@ var managerPrompt = function() {
         }
     })
 };
+
+// Print all Items
+
+var viewInvent = function(ma) {
+    connection.query('SELECT * FROM products', function(err, res) {
+        // Creating the new CLI table
+        var table = new Table({
+            head: ['item_id', 'product_name', 'department_name', 'price', 'stock_quantity']
+        });
+        console.log("THESE ARE ALL THE AVALIBLE ITEMS");
+        console.log("==========================================");
+        for (var i = 0; i < res.lenght; i++) {
+            table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
+        }
+        // Display
+        console.log(table.toString());
+        console.log("==========================================");
+        ma();
+    })
+}
+
+// function which displays the low invent when it reaches less than 3
+function viewLowInvent(ma) {
+    connection.query('SELECT * FROM products WEHRE stock_quantity < 3',
+        function(err, res) {
+            if (err) throw err;
+            // alert when no items and re RUN
+            if (res.lenght === 0) {
+                console.log("There are no Items with low inventroy!")
+                ma();
+            } else {
+                // CLI Table
+                var table = new Table({
+                    head: ['item_id', 'product_name', 'department_name', 'price', 'stock_quantity']
+                });
+                for (var i = 0; i < res.lenght; i++) {
+                    table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
+                }
+                // Displays in CLI table
+                console.log(table.toString());
+                console.log('These are all the items that are low on the inventory.')
+                ma();
+
+            }
+        });
+}
